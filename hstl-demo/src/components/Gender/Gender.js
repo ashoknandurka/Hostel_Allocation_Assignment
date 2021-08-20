b7765eb33ab1f4e26904753f25dff128ea73a76e
ashoknandurka <86875391+ashoknandurka@users.noreply.github.com>
2021-08-20 18:39:07 +0530

    first project

---
+++ hstl-demo/src/components/Gender/Gender.js
@@ -0,0 +1,12 @@
+import React from 'react'
+  
+
+const Gender = () =>{
+
+    return(
+        <>
+        <button>Boy</button>
+        <button>Girl</button>
+        </>
+    )
+}
---
+++ hstl-demo/src/components/nothing.js
@@ -0,0 +1,30 @@
+// import React, {useState} from 'react'
+
+// const Login =() => {
+//     const [email, SetEmail] = useState("");
+//     const [password,setPassword] = useState("");
+//     return (
+//        <>
+//        <form className="login-form">
+//            <div>
+//                <label htmlFor="email">Email</label>
+//                <input type="text" name="email" id="email" autoComplete="off" 
+//                value={email} 
+//                onChange={(e) => SetEmail(e.target.value)} />
+//            </div>
+
+//            <div>
+//                <label htmlFor="password">password</label>
+//                <input type="password" name="password" id="password" autoComplete="off" 
+//                 value={password}
+//                 onChange={(e) => setPassword(e.target.value)} />
+//            </div>
+
+//            <button type="submit">Login</button>
+//        </form>
+       
+//        </>
+//     )
+// }
+
+// export default Login
